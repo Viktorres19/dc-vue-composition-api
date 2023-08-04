18693178261afab3becdc9f20aa52b4da6b293f0
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2023-08-04 14:06:22 +0300

    44. Child Components

---
+++ src/components/Modal.vue
@@ -0,0 +1,22 @@
+<template>
+	<teleport to="body">
+		<div
+			class="modal"
+		>
+			<h2>This is a modal</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias amet animi consectetur culpa eius enim eveniet fugit impedit incidunt ipsum, itaque, iusto laudantium pariatur quas quidem quo tempore veniam vero voluptates? Accusantium aspernatur consequatur dignissimos dolor eaque, fuga illo illum laborum maiores nisi officia pariatur sed temporibus veritatis voluptatum?</p>
+			<button>Hide modal</button>
+		</div>
+	</teleport>
+</template>
+
+<style>
+.modal {
+	background: beige;
+	padding: 10px;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+}
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,29 +2,12 @@
 	<div class="modals">
 			<h1>Modals</h1>
 			<button @click="showModal = true">Show modal</button>
-			<teleport to="body">
-			  <div
-				  v-if="showModal"
-				  class="modal"
-			  >
-				  <h2>This is a modal</h2>
-				  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias amet animi consectetur culpa eius enim eveniet fugit impedit incidunt ipsum, itaque, iusto laudantium pariatur quas quidem quo tempore veniam vero voluptates? Accusantium aspernatur consequatur dignissimos dolor eaque, fuga illo illum laborum maiores nisi officia pariatur sed temporibus veritatis voluptatum?</p>
-				  <button @click="showModal = false">Hide modal</button>
-			  </div>
-			</teleport>
+			<Modal v-if="showModal" />
 	</div>
 </template>
 <script setup>
 import { ref } from 'vue'
+import Modal from "@/components/Modal.vue";
 const showModal = ref(false)
 </script>
-<style>
-.modal {
-	background: beige;
-	padding: 10px;
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-}
-</style>
+
