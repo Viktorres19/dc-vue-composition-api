ccb7119e537b024ce74b941ab0e5c34c0f845d61
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2023-08-08 20:18:45 +0300

    Section 12 State Management with Pinia

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <h2 ref="appTitleRef">{{ appTitle }}</h2>
-    <h3>{{ counterData.title }}:</h3>
+    <h3>Title: {{ counter.title }}</h3>
     <div>
-      <button @click="changeCounter(-1)" class="btn">-</button>
-      <span class="counter">{{ counterData.count }}</span>
-      <button @click="changeCounter(1)" class="btn">+</button>
+      <button @click="counter.decrement()" class="btn">-</button>
+      <span class="counter">{{ counter.count }}</span>
+      <button @click="counter.increment()" class="btn">+</button>
     </div>
 
-    <p>This counter is: {{ oddOrEven }}</p>
+    <p>This counter is: {{ counter.evenOrOdd }}</p>
 
     <div class="edit">
       <h4>Edit counter title:</h4>
-      <input v-model="counterData.title" type="text" v-autofocus>
+      <input v-model="counter.title" type="text" v-autofocus>
     </div>
 
   </div>
@@ -31,22 +31,17 @@ import {
   onUpdated,
   ref
 } from 'vue'
-import { useCounter } from '@/use/useCounter'
+import { useCounterStore } from '@/stores/counter'
 import vAutofocus from '@/directives/vAutofocus'
 
-/*const counter = ref(0),
-      counterTitle = ref('My Counter')*/
-
-// Non-reactive data
 const appTitle = "My Amazing Counter App"
 
-// access to refs
 const appTitleRef = ref(null)
 
 onBeforeMount(() => {
   console.log('onBeforeMount')
 })
-//we can use several onMounted for example hooks
+
 onMounted(() => {
   //then call this ref
   console.log(`The app title is ${appTitleRef.value.offsetWidth} px wide!`)
@@ -69,8 +64,7 @@ onBeforeUpdate(() => {
 onUpdated(() => {
   console.log('onUpdated')
 })
-
-const { counterData, oddOrEven, changeCounter } = useCounter()
+const counter = useCounterStore()
 </script>
 
 <style>
